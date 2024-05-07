81aaf08c99a17017edcf93fd0109b028b42d6e41
ankitgala11 <ankit.g@stepapp.ai>
2024-05-07 18:29:33 +0530

    added redux toolkit and redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { error, loading } = useSelector((state) => state.user);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleData = (e) => {
     setFormData({
@@ -18,7 +26,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    setLoading(true);
+    dispatch(signInStart());
     console.log(formData);
 
     const res = await fetch("/api/auth/signin", {
@@ -33,13 +41,11 @@ export default function SignIn() {
     console.log(data);
 
     if (data.success === false) {
-      setLoading(false);
-      setError(data.message);
+      dispatch(signInFailure(data.message));
       return;
     }
+    dispatch(signInSuccess(data));
 
-    setLoading(false);
-    setError(null);
     navigate("/");
   };
 
