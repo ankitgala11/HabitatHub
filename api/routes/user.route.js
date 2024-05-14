c60cea299419874875843507d8f73fa783dcbb8e
ankitgala11 <ankit.g@stepapp.ai>
2024-05-14 15:45:29 +0530

    added show listing api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {
-	test,
-	deleteUser,
-	updateUser,
+  test,
+  deleteUser,
+  updateUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
