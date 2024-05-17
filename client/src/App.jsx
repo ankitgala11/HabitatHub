6d225ce046ed2465ff891c2ffe00448f922cfbb5
Ankit Gala <ankitgala.11.ag@gmail.com>
2024-05-18 02:03:47 +0530

    complete edit and delete listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,28 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/about" element={<About />} />
 
-        <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+				<Route element={<PrivateRoute />}>
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/create-listing" element={<CreateListing />} />
+					<Route
+						path="/update-listing/:listingId"
+						element={<UpdateListing />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
