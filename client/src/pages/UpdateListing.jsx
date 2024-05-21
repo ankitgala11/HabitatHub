faaf08ea2ee5285d346ddebdc435b3a76cec5eb2
Ankit Gala <ankitgala.11.ag@gmail.com>
2024-05-21 18:13:31 +0530

    Complete search and show listing

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -306,8 +306,10 @@ export default function CreateListing() {
 							/>
 							<div className="flex flex-col items-center">
 								<p>Regular price</p>
-								{formData.type === "rent" && (
-									<span className="text-xs">($ / month)</span>
+								{formData.type === "rent" ? (
+									<span className="text-xs">(₹ / month)</span>
+								) : (
+									<span className="text-xs">( ₹ )</span>
 								)}
 							</div>
 						</div>
@@ -325,10 +327,12 @@ export default function CreateListing() {
 								/>
 								<div className="flex flex-col items-center">
 									<p>Discounted price</p>
-									{formData.type === "rent" && (
+									{formData.type === "rent" ? (
 										<span className="text-xs">
-											($ / month)
+											(₹ / month)
 										</span>
+									) : (
+										<span className="text-xs">( ₹ )</span>
 									)}
 								</div>
 							</div>
