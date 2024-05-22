e80e76fc6b1e5d90abdda9742f7725fcb4fb26e6
Ankit Gala <ankitgala.11.ag@gmail.com>
2024-05-23 01:18:13 +0530

    removed footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import PrivateRoute from "./components/PrivateRoute";
 import Search from "./pages/Search";
-import Footer from "./components/Footer";
 
 export default function App() {
 	return (
@@ -33,7 +32,6 @@ export default function App() {
 					/>
 				</Route>
 			</Routes>
-			<Footer />
 		</BrowserRouter>
 	);
 }
